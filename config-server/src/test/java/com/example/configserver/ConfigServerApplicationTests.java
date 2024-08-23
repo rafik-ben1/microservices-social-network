31707ebaad8eedfcd04269f994ac515fb29c96cc
rafik-ben1 <gokub531@gmail.com>
2024-08-23 17:01:17 +0100

    dockerizing the app

--- config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.configservice;
+package com.example.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServiceApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
