0cc4b919f5cb226bb155db028497c52f76c59b34
rafik-ben1 <gokub531@gmail.com>
2024-10-22 15:43:22 +0100

    adding skeleton for user profile

---
+++ frontend/src/features/users/UserProfileSkeleton.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import { CardHeader } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+
+const UserProfileSkeleton = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <CardHeader className="flex flex-row items-center gap-4">
+        <Button
+          onClick={() => navigate(-1)}
+          className="md:hidden"
+          variant="ghost"
+          size="icon"
+        >
+          {" "}
+          <ArrowLeft />{" "}
+        </Button>
+        <Skeleton className="w-20 h-20 rounded-full " />
+        <div className="flex flex-col flex-grow">
+          <div className="flex justify-between items-center">
+            <div>
+              <Skeleton className="w-28 p-4 mb-2 " />
+              <Skeleton className="w-16 p-2 " />
+            </div>
+            <Skeleton className="w-20 h-8" />
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid gap-2">
+          <h3 className="font-semibold">Bio</h3>
+           <Skeleton className=" w-full h-10 " />
+        </div>
+        <div className="grid gap-2">
+          <h3 className="font-semibold">Details</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-sm">
+            <div className="flex items-center gap-2">
+              <Skeleton className="h-6 w-10"  />
+            </div>
+            <Skeleton className="p-3 "/>
+            <Skeleton className="p-3"/>
+            <Skeleton className="p-3" />
+          </div>
+        </div>
+        <div className="grid gap-2">
+          <h3 className="font-semibold">Interests</h3>
+          <div className="flex flex-wrap gap-2">
+            <Skeleton className="w-12 h-8 rounded-sm" />
+            <Skeleton className="w-12 h-8 rounded-sm "/>
+            <Skeleton className="w-12 h-8 rounded-sm "/>
+            <Skeleton className="w-12 h-8 rounded-sm "/>
+            <Skeleton className="w-12 h-8 rounded-sm "/>
+          </div>
+        </div>
+      </CardContent>
+    </>
+  );
+};
+
+export default UserProfileSkeleton;
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -2,18 +2,20 @@ import { Card } from "@/components/ui/card"
 import { UserProfileHeader } from "@/features/users/UserProfileHeader"
 import { UserProfileAttributes } from "@/features/users/UserProfileAttributes"
 import { useGetUser } from "@/features/users/UserService"
+import UserProfileSkeleton from "@/features/users/UserProfileSkeleton"
 export default function Profile() {
  
   
   const {data,isLoading}  = useGetUser()
-
-  if(isLoading){
-    return <div>loading...</div>
-  }
   return (
     <Card className= "grow rounded-none sm:p-4 ">
-     <UserProfileHeader  user={data!} />
-      <UserProfileAttributes />
+     {isLoading ? <UserProfileSkeleton /> 
+      : ( 
+	 <>
+          <UserProfileHeader  user={data!} />
+          <UserProfileAttributes />
+        </>
+	)}
     </Card>
   )
 }
