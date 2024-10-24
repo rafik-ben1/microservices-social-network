adba4f3b3283485d180bc1cd8fae2b7fcadf3c4c
rafik-ben1 <gokub531@gmail.com>
2024-10-24 15:31:33 +0100

    small ui change for my friends

--- frontend/src/features/friends/MyFriends.tsx
+++ frontend/src/features/friends/MyFriends.tsx
@@ -1,17 +1,18 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useGetMyFriends } from "./FriendService";import UserRowSkeleton from "../users/UserRowSkeleton";
 import { Button } from "@/components/ui/button";
+import {useNavigate} from "react-router-dom"
 
 const MyFriends = () => {
   const { data, isLoading } = useGetMyFriends();
-
+  const navigate = useNavigate()
   if (isLoading) {
     return <UserRowSkeleton />
   }
   return (
     <>
       {data?.content.map((friend) => (
-        <div key={friend.id} className="flex items-center justify-between py-2">
+        <div onClick={()=> navigate("/profile/"+friend.id)} key={friend.id} className="flex items-center justify-between py-2">
           <div className="flex items-center">
             <Avatar className="h-8 w-8">
               <AvatarImage src={friend.avatar} alt={friend.username} />
@@ -21,9 +22,6 @@ const MyFriends = () => {
               {friend.firstname + " " + friend.lastname}
             </span>
           </div>
-          <Button variant="destructive" >
-            unfriend
-          </Button>
         </div>
       ))}
     </>
