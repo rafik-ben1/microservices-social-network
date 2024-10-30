ea08e448923e886fd88a87b4d4679db77f55ba03
rafik-ben1 <gokub531@gmail.com>
2024-10-30 14:30:42 +0100

    adding missing enable async annotation

--- post-service/src/main/java/com/example/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/example/postservice/PostServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableAsync
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
